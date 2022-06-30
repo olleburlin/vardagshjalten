138f19ad9106ae37c21e5f76c86aef0304b2b9c0
Olle Burlin <burlinolle@gmail.com>
2022-06-30 12:25:08 +0200

    Commit

--- src/components/index/Hemstadning.js
+++ src/components/index/Hemstadning.js
@@ -4,7 +4,7 @@ import Button from "../common/Button"
 export default function Hemstadning() {
   return (
     <div className="bg-secondary py-8 md:py-16">
-      <div className="max-w-7xl mx-auto container px-4 md:px-8">
+      <div className="max-w-screen-2xl mx-auto container px-4 md:px-8">
         <div className="flex flex-col gap-8 md">
           <h3 className="">Hemstädning</h3>
           <p className="text-3xl md:text-7xl text-white font-bold">
