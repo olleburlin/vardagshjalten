5fea21e0ac551de6f73d95c9b920b0d142cf1289
Olle Burlin <burlinolle@gmail.com>
2022-08-23 13:06:29 +0200

    Commit

--- src/components/index/Kampanj.js
+++ src/components/index/Kampanj.js
@@ -1,59 +1,75 @@
-import { StaticImage } from "gatsby-plugin-image"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import React from "react"
 import Button from "../common/Button"
 import SectionHeader from "../common/SectionHeader"
+import { useStaticQuery, graphql } from "gatsby"
 
 export default function Kampanj() {
+  const data = useStaticQuery(graphql`
+    {
+      wpPage(databaseId: { eq: 40 }) {
+        id
+        kampanj {
+          asteriskKampanj
+          huvudrubrikKampanj
+          textKampanj
+          underrubrikKampanj
+          bildKampanj {
+            localFile {
+              childImageSharp {
+                gatsbyImageData
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+  const {
+    asteriskKampanj,
+    huvudrubrikKampanj,
+    textKampanj,
+    underrubrikKampanj,
+    bildKampanj,
+  } = data.wpPage.kampanj
+
   return (
     <div className="bg-white">
       <div className="container max-w-screen-2xl mx-auto px-4 md:px-8 py-8 md:py-16 space-y-4 ">
         <div className="flex  flex-col lg:flex-row items-start gap-8 md:gap-16 my-8 w-full">
           <div className="w-full lg:w-1/2 order-last lg:order-first">
             {" "}
-            <StaticImage
-              src="../../images/hund.jpeg"
-              quality={70}
+            <GatsbyImage
+              image={getImage(bildKampanj.localFile.childImageSharp)}
+              alt="Hund"
               className="h-full"
             />
           </div>
           <div className="w-full lg:w-1/2 flex flex-col justify-center  text-xl">
             <div className="lg:-mt-8">
-              <SectionHeader text="Kampanjerbjudande" theme="dark" />
+              <SectionHeader text={huvudrubrikKampanj} theme="dark" />
             </div>
             <div className="space-y-4 lg:space-y-8">
               <div className="text-3xl lg:text-7xl font-bold leading-none">
-                <div className="leading-snug">
-                  Dags att flytta? <br />
-                  Vi hjälper dig!
+                <div className="leading-snug xl:pr-40">
+                  {underrubrikKampanj}{" "}
                 </div>
               </div>
               <div className="space-y-4">
-                <p>Just nu erbjuder vi 10% rabatt på flyttstädning.</p>
-                <p>
-                  Inför en flytt har du ofta fullt upp med alla bestyr och de
-                  flesta vill inte lägga ner åtskilliga timmars arbete på
-                  städning när man har ett nytt hem att ta hand om. Det ställs
-                  höga krav på flyttstädning. Låt oss hjälpa dig och ta hand om
-                  städningen. Du spar både tid och bekymmer och med RUT–avdraget
-                  blir det inte så kostsamt. Naturligtvis har du garanti på
-                  utfört arbete. Personalen från Vardagshjälten har med sig allt
-                  material de behöver, så det är inget du behöver tänka på.
-                </p>
-                <p>
-                  Vi på Vardagshjälten lämnar alltid garanti efter oss som
-                  gäller i 24 timmar efter städtillfället. Alla hushållsnära
-                  tjänster ingår i skattereduktionen som innebär att ni alltid
-                  endast betalar 50 % av kostnaderna.
-                </p>
+                <div
+                  dangerouslySetInnerHTML={{ __html: textKampanj }}
+                  className="wp-content"
+                />
               </div>
               <div>
                 <Button text="Få offert" url="/offert" />
               </div>
               <div>
                 <p className="text-sm text-gray-500">
-                  * Alla priser är beräknade efter rutavdrag. Erbjudandet kan
-                  inte kombineras med andra rabatter eller erbjudanden. Du
-                  behöver boka erbjudandet senast 31 oktober 2022.
+                  <div
+                    dangerouslySetInnerHTML={{ __html: asteriskKampanj }}
+                    className="wp-content"
+                  />
                 </p>
               </div>
             </div>
