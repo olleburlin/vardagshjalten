76ccafcd856a57f1ccbe0a46ed910b6877b0bf20
Olle Burlin <burlinolle@gmail.com>
2023-06-07 10:58:37 +0200

    Commit

--- src/components/common/OfferForm.js
+++ src/components/common/OfferForm.js
@@ -64,8 +64,6 @@ export default function OfferForm() {
                 <option value="Hemstädning">Hemstädning</option>
                 <option value="Storstädning">Storstädning</option>
                 <option value="Flyttstädning">Flyttstädning</option>
-                <option value="Fönsterputs">Fönsterputs</option>
-                <option value="Trädgårdsarbete">Trädgårdsarbete</option>
                 <option value="Fastighetsskötsel">Fastighetsskötsel</option>
                 <option value="Kontorsstäd">Kontorsstäd</option>
                 <option value="Kontorsstäd">Byggstäd</option>
